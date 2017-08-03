40e150a77b65718a30359eeca7286202e9bdfbf4
Alexandr Lazarev <alexandr_lazarev@bonify.de>
2017-08-03 11:45:36 +0200

    Fixed typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function setAmpCORS() {
   return  async function setAmpCORS(ctx, next) {
     await next();
     const ampSourceOrigin = ctx.query['__amp_source_origin']
-    if (!ampSourceOrigin) throw new Error('Now __amp_source_origin param provided')
+    if (!ampSourceOrigin) throw new Error('No __amp_source_origin param provided')
     ctx.set('AMP-Access-Control-Allow-Source-Origin', ampSourceOrigin);
     ctx.set('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
     ctx.set('Access-Control-Allow-Origin', ampSourceOrigin);
