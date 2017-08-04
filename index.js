f5325a80aff693f3a69055db35f32be4f6c7ac4d
Alexandr Lazarev <alexandr_lazarev@bonify.de>
2017-08-04 12:27:21 +0200

    Implemented all verifications

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-module.exports = function setAmpCORS() {
+module.exports = function setAmpCORS(options = {}) {
   return  async function setAmpCORS(ctx, next) {
     await next();
     const ampSourceOrigin = ctx.query['__amp_source_origin']
-    if (!ampSourceOrigin) throw new Error('No __amp_source_origin param provided')
+    const origin = ctx.headers['origin']
+    if (!origin && !ctx.headers['amp-same-origin']) {
+      throw new Error('No Origin header is provided')
+    }
+
+    if (origin) {
+      if (options.origin && options.origin !== origin || !checkAmpDomains(origin)) {
+        throw new Error('Wrong Origin header provided')
+      }
+      if (!checkAmpDomains(origin)) {
+          throw new Error('Wrong Origin header provided')
+      }
+    }
+
+    if (!ampSourceOrigin) {
+        throw new Error('No __amp_source_origin param is provided')
+    }
     ctx.set('AMP-Access-Control-Allow-Source-Origin', ampSourceOrigin);
     ctx.set('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
     ctx.set('Access-Control-Allow-Origin', ampSourceOrigin);
     ctx.set('Access-Control-Allow-Credentials', 'true');
   }
 };
+
+function checkAmpDomains(origin) {
+  return origin.endsWith('.ampproject.org') || origin.endsWith('.amp.cloudflare.com')
+}
